c4e7d4c41ed6daa997f42672da9e3a8db47523eb
Luuk <luukhafkamp@gmail.com>
2019-01-18 17:56:24 +0100

    Set up shop

---
+++ server.js
@@ -0,0 +1,10 @@
+const { GraphQLServer } = require('graphql-yoga')
+const Query = require('./resolvers/Query')
+const port = 'http://localhost:4000'
+
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers: Query
+})
+
+server.start(() => console.log(`Server is running on ${port}`))
